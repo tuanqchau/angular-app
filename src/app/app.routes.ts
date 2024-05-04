512764218e8f8ac8f4bf87177742be17f95c78b6
Tuan Chau <tuanqchau2810@gmail.com>
2024-05-04 12:12:54 -0400

    getting items from backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 export const routes: Routes = [
     {
-        path: 'home',
+        path: '',
         component: HomeComponent,
     }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  constructor(
+    private productService: ProductsService
+  ) {
+
+  }
+
+  ngOnInit() {
+    //when this HomeComponent is first initialized, ngOnInit will run
+    this.productService
+    .getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5})
+    .subscribe((products: Products) => {
+      console.log(products.items);
+    })
+  }
 }
